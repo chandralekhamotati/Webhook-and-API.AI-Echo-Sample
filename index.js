ca4966a7f4a72a7661b8c2922a79fd4b91f7efa6
chandralekhamotati <chandralekhamotati@users.noreply.github.com>
2017-05-31 16:39:03 -0700

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ restService.post('/echo', function(req, res) {
         speech: speech,
         displayText: speech,
         source: 'webhook-echo-sample'
-        title:'Test card'
+        
     });
 });
 
